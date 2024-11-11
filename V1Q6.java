17bb2355d18dd31c799a658ddc89b3a89d28cb86
Jrlong <150505265+Jlong0@users.noreply.github.com>
2024-11-11 12:08:20 +0800

    Add files via upload

---
+++ V1Q6.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package v1q6;
+import java.util.Scanner;
+/**
+ *
+ * @author jirui
+ */
+public class V1Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int temp, highestNum = 0, highestCount = 0, secondHighNum = 0, secondHighCount = 0, sum = 0;
+        boolean checkNegative = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter score: ");
+        highestNum = sc.nextInt();
+        highestCount++;
+        sum =+ highestNum;
+        while(true){
+            temp = sc.nextInt();
+            
+            sum += temp;
+            if(temp == 0){
+                    break;
+            }
+            else if(temp == highestNum){
+                highestCount++;
+            }
+            else if(temp == secondHighNum){
+                secondHighCount++;
+            }
+            else if(temp > highestNum){
+                
+                secondHighNum = highestNum;
+                highestNum = temp;
+                secondHighCount = highestCount;
+                highestCount = 1;
+                
+            }
+            else if(temp < highestNum && temp > secondHighNum){
+                
+                secondHighNum = temp;
+                secondHighCount = 1;
+                
+            }
+            
+            if (temp<0){
+                checkNegative = true;
+            }
+            
+        }
+        System.out.println("The largest number is " + highestNum);
+        System.out.println("The occurence count of the largest number is " + highestCount);
+        System.out.println("The second-largest number is " + secondHighNum);
+        System.out.println("The occurence count of the second-largest number is " + secondHighCount);
+        System.out.println("The total sum of all numbers is " + sum);
+        if(checkNegative){
+            System.out.println("Negative numbers were entered.");
+        }
+        else{
+            System.out.println("Negative numbers were not entered");
+        }
+    }
+    
+}
